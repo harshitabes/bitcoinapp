759b3daa59a3c128c428cc56cb8d445b71617b14
harshitabes <harshit.15bcs1139@abes.ac.in>
2018-07-09 17:30:29 +0530

    first commit3

--- bitcoin/views.py
+++ bitcoin/views.py
@@ -5,23 +5,57 @@ from .models import Person
 def home(request):
     var=Person.objects.all()
     context={'data':var}
-    return render(request,'bitcoin/bit.html',context)
+    return render(request,'bitcoin/bit.html', context)
 
 
 def price(request):
     if request.method == 'POST':
         var = Person(username=request.POST['name'], currency=request.POST['Currency'])
         var.save()
-        #return redirect('price')
+
     r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+
+
+
+
+
+    url = 'https://v3.exchangerate-api.com/bulk/f5e10faef5cbc261feeab7fb/'
+    con='USD'
+    url += con
+
+
+
+    response = requests.get(url)
+    val = response.json()
+
+
+
 
     y = r.json()['bpi']
 
-    '''for key, value in y.items():
-        print(key,value['rate'])'''
+
 
     detail=var.currency
-    data = y[detail]['rate']
-    context={'data': data,'detail':detail}
 
-    return render(request, 'bitcoin/price.html', context)
+
+
+    if detail == 'INR':
+        data = y['USD']['rate']
+        for i in range(0, len(data)):
+            if data[i] == ',':
+                pos = i
+                break;
+
+        s1 = ""
+        s1 += data[0:pos]
+        s1 += data[pos + 1:len(data)]
+        s4 = float(s1)
+        data = str(s4*val['rates']['INR'])
+
+        context={'data': data, 'detail':detail}
+        return render(request, 'bitcoin/price.html', context)
+
+    else:
+        data = y[detail]['rate']
+        context = {'data': data, 'detail': detail,}
+        return render(request, 'bitcoin/price.html', context)
